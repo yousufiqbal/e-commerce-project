5050a495d94ddf3384dbe6d436e2895ddb2e8a0a
Yousuf Iqbal <yousufiqbalhahim@gmail.com>
2022-05-28 00:16:03 +0500

    uih

--- src/lib/components/AddToCart.svelte
+++ src/lib/components/AddToCart.svelte
@@ -1,20 +1,33 @@
 <script>
   import Counter from "$lib/components/Counter.svelte";
 
+  export let product_id
   export let counts = 0
   export let limit = 100
 
+  const updateCart = () => {
+
+  }
+
   const increase = () => {
+    if (counts >= limit) return
     counts++
+  }
+
+  const decrease = () => {
+    if (counts == 0) return
+    counts--
   }
 </script>
 
 <div class="add-to-cart">
+
   {#if counts == 0}
   <button on:click={increase}>Add To Cart</button>
   {:else}
-  <Counter bind:counts {limit} />
+  <Counter on:increase={increase} on:decrease={decrease} {counts} />
   {/if}
+
 </div>
 
 <style>
