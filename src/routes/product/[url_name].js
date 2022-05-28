54d382d9e96ea03ed34f695d324f7fa138bd68cc
Yousuf Iqbal <yousufiqbalhahim@gmail.com>
2022-05-29 00:43:37 +0500

    cart stuff

--- src/routes/product/[url_name].js
+++ src/routes/product/[url_name].js
@@ -3,8 +3,6 @@ import { db } from '$lib/database/db'
 /** @type {import('@sveltejs/kit').RequestHandler} */
 export const get = async ({ params }) => {
 
-  console.log(params.url_name)
-
   const product = await db.selectFrom('products')
     .select(['products.product_id', 'products.name', 'products.url_name', 'products.price', 'products.description', 'products.stock', 'products.fair_quantity'])
     .where('products.url_name', '=', params.url_name)
