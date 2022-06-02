416e29c45626b3a61c891a664f488e5d604a7e87
Yousuf Iqbal <yousufiqbalhashim@gmail.com>
2022-06-02 16:09:54 +0500

    kjn

--- src/routes/account/about.svelte
+++ src/routes/account/about.svelte
@@ -1,5 +1,7 @@
 <script>
   import Breadcrumb from "$lib/components/Breadcrumb.svelte";
+import Subtitle from "$lib/components/Subtitle.svelte";
+import Text from "$lib/components/Text.svelte";
   import Title from "$lib/components/Title.svelte";
 
   const crumbs = [
@@ -10,3 +12,21 @@
 
 <Breadcrumb {crumbs} />
 <Title back title="About" />
+
+<Subtitle subtitle="Info" />
+
+<Text>
+  Fast.com is a properitorship company. We provide e-commerce (SAAS) Software As A Service, on custom requirements.
+</Text>
+
+<Subtitle subtitle="Office Location" />
+
+<Text>
+  G5, Nadi Par, Jhangru Bhangru, Karachi
+</Text>
+
+<Subtitle subtitle="Rights" />
+
+<Text>
+  Lorem ipsum dolor sit amet consectetur, adipisicing elit. Explicabo nemo doloremque veniam harum odio. Illo quo ipsa impedit magnam eveniet! &copy; { new Date().getFullYear()}
+</Text>
--- src/routes/account/customer-support.svelte
+++ src/routes/account/customer-support.svelte
@@ -1,5 +1,10 @@
 <script>
   import Breadcrumb from "$lib/components/Breadcrumb.svelte";
+import Message from "$lib/components/Message.svelte";
+import SmallButton from "$lib/components/SmallButton.svelte";
+import SmallButtonGroup from "$lib/components/SmallButtonGroup.svelte";
+import Subtitle from "$lib/components/Subtitle.svelte";
+import Text from "$lib/components/Text.svelte";
   import Title from "$lib/components/Title.svelte";
 
   const crumbs = [
@@ -11,3 +16,69 @@
 <Breadcrumb {crumbs} />
 
 <Title back title="Customer Support" />
+
+<Message pinned>
+  Our customer support representatives are available <strong>9AM to 6PM</strong> everyday (except Sunday and prayer timings).
+</Message>
+
+<div class="supports">
+
+  <div class="card">
+    <Subtitle subtitle="WhatsApp" icon="whatsapp" />
+  
+    <Text>
+      You can chat with us on WhatsApp No. <strong>0333-3535658</strong> regarding your queries
+    </Text>
+
+    <Text>
+      <i>
+        Please do not call. We only support chat on WhatsApp.
+      </i>
+    </Text>
+    
+    <SmallButtonGroup>
+      <SmallButton icon="messageThree" name="Chat Now" />
+    </SmallButtonGroup>
+  </div>
+  
+  <div class="card">
+    <Subtitle subtitle="Email" icon="mail" />
+    
+    <Text>
+      You can email us on <strong>support@fast.com</strong> regarding your queries.
+    </Text>
+    
+    <SmallButtonGroup>
+      <SmallButton icon="mailSend" name="Email Now" />
+    </SmallButtonGroup>
+  </div>
+  
+  <div class="card">
+    <Subtitle subtitle="Phone" icon="cellphone" />
+  
+    <Text>
+      You can also call us on <strong>021-34546549</strong> regarding your queries.
+    </Text>
+    
+    <SmallButtonGroup>
+      <SmallButton icon="phone" name="Call Now" />
+    </SmallButtonGroup>
+  </div>
+  
+</div>
+
+<div class="mb40"></div>
+
+<style>
+  i {
+    font-style: italic;
+  }
+  strong {
+    white-space: nowrap;
+    font-weight: bold;
+  }
+  .supports {
+    display: grid;
+    gap: 20px;
+  }
+</style>
