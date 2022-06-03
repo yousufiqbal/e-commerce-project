094e9977df61e653a423cf2124f887dc8e1d285d
Yousuf Iqbal <yousufiqbalhahim@gmail.com>
2022-06-03 09:01:35 +0500

    asd

--- src/routes/api/orders/index.js
+++ src/routes/api/orders/index.js
@@ -17,16 +17,44 @@ export const post = async ({ request, locals }) => {
     .where('users.user_id', '=', locals.user_id)
     .executeTakeFirst()
 
-  // Check promo validity.. etc..
+  // TODO Check promo validity.. product, stock, price, cost.. etc..
 
   // TODO transaction..
-  const { insertId } = await db.insertInto('orders').values({
-    address: completeAddress,
-    payment_method,
-    promo_id: user.applied_promo_id,
-    user_id: locals.user_id
-  }).executeTakeFirst()
 
-  return { status: 201, body: { message: Number(insertId) } }
+    await db.transaction().execute(async trx => {
+    
+      // Add order
+      const { insertId } = await trx.insertInto('orders').values({
+        address: completeAddress,
+        payment_method,
+        promo_id: user.applied_promo_id,
+        user_id: locals.user_id
+      }).executeTakeFirstOrThrow()
+  
+      const order_id = Number(insertId)
+  
+      // Shifting cart_items to order_details (requires products table)
+      const cartItems = await trx.selectFrom('cart_items')
+        .leftJoin('products', 'products.product_id', 'cart_items.product_id')
+        .where('cart_items.user_id', '=', locals.user_id)
+        .select(['cart_items.product_id', 'cart_items.quantity', 'products.unit_cost', 'products.price'])
+        .execute()
+  
+      const details = []
+  
+      cartItems.forEach(item => {
+        details.push({...item, order_id})
+      })
+  
+      await trx.insertInto('order_details').values(details).execute()
+
+      // Removing cart_items and applied_promo_code 
+      await db.updateTable('users').where('users.user_id', '=', locals.user_id)
+      .set({ applied_promo_id: null }).execute()
+      await db.deleteFrom('cart_items').where('cart_items.user_id', '=', locals.user_id).execute()
+  
+    })
+ 
+  return { status: 201, body: { message: order_id } }
 
 }
