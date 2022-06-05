4601a71978fd48c2544f2b3a8cad57ee3894bbcc
Yousuf Iqbal <yousufiqbalhahim@gmail.com>
2022-06-05 14:12:53 +0500

    added tabs to messages

--- src/routes/account/messages.svelte
+++ src/routes/account/messages.svelte
@@ -6,6 +6,7 @@
   import { dev } from "$app/env";
   import { onMount } from "svelte";
   import { invalidate } from "$app/navigation";
+import QueryTabs from "$lib/components/QueryTabs.svelte";
   
   // const getUnread = getContext('getUnread')
 
@@ -26,10 +27,19 @@
     { name: 'Messages', href: '/account/messages' },
   ]
 
+  const tabs = [
+    { name: 'All', url_name: 'all' },
+    { name: 'Orders', url_name: 'orders'},
+    { name: 'Promos', url_name: 'promos'},
+    { name: 'Offers', url_name: 'offers'},
+  ]
+
   export let messages = []
 </script>
+
 
 <Breadcrumb {crumbs} />
 <Title back title="Messages" />
 
+<QueryTabs name="type" items={tabs} />
 <Messages {messages} />
