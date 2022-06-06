3ccb8d4a59a2550395b9d76a6d7e3b61fda633fd
Yousuf Iqbal <yousufiqbalhashim@gmail.com>
2022-06-06 15:22:47 +0500

    asdf

--- admin-panel/src/routes/products/index.svelte
+++ admin-panel/src/routes/products/index.svelte
@@ -1,6 +1,7 @@
 <script>
   import Button from "$lib/components/Button.svelte";
   import ButtonGroup from "$lib/components/ButtonGroup.svelte";
+import Nothing from "$lib/components/Nothing.svelte";
   import Title from "$lib/components/Title.svelte";
 </script>
 
@@ -10,3 +11,7 @@
   <Button icon="add" name="New" href="/products/add-product" />
   <Button icon="add" name="Add Stock" href="/products/add-stock" />
 </ButtonGroup>
+
+<Nothing>
+  No Products
+</Nothing>
