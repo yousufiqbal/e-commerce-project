4b7c417ecfb6867762c63fe5e78fdf84ff57ea29
Yousuf Iqbal <yousufiqbalhahim@gmail.com>
2022-06-06 07:18:44 +0500

    start..

---
+++ admin-panel/src/hooks.js
@@ -0,0 +1,42 @@
+import { dev } from '$app/env'
+// import { db } from '$lib/database/db'
+// import cookie from 'cookie'
+// import jwt from 'jsonwebtoken'
+
+/** @type {import('@sveltejs/kit').Handle} */
+export const handle = async ({ event, resolve }) => {
+
+  // Log URL & Method
+  if (dev)  {
+    console.log('----------------')
+    console.log(event.request.method, event.request.url)
+  }
+  
+  // // Cookie
+  // const cookies = cookie.parse(event.request.headers.get('cookie') || '')
+  // const fact = cookies.fact || null
+  // const secret = import.meta.env.VITE_SECRET
+
+  // if (fact && jwt.verify(fact, secret)) {
+    
+  //   // User or Guest..
+  //   const payload = jwt.decode(fact)
+  //   if (payload.user_id) event.locals = payload
+  //   if (payload.guest_id) event.locals = payload
+
+  // } else {
+  //   // Not Both..
+  //   event.locals = {}
+  // }
+
+  // Response
+  return await resolve(event)
+
+}
+
+// /** @type {import('@sveltejs/kit').GetSession} */
+// export const getSession = async ( event ) => {
+
+//   return event.locals
+
+// }
