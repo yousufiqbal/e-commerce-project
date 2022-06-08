cf249beed850abce750e376aa8642e05f0dad889
Yousuf Iqbal <yousufiqbalhahim@gmail.com>
2022-06-08 23:38:33 +0500

    asdf

--- admin-panel/src/lib/components/NProgress.svelte
+++ admin-panel/src/lib/components/NProgress.svelte
@@ -2,9 +2,30 @@
   import NProgress from 'nprogress'
   import { navigating } from '$app/stores'
   import 'nprogress/nprogress.css'
+
+  // TODO other delay strategy
+  NProgress.doStart = NProgress.start;
+  NProgress.doDone = NProgress.done;
+  NProgress.clearDelay = function () {
+    if (this.startDelay) {
+      clearTimeout(this.startDelay);
+      this.startDelay = undefined;
+    }
+  }
+  NProgress.start = function () {
+    this.clearDelay();
+    this.startDelay = setTimeout(function () {
+      NProgress.doStart();
+    }, this.settings.delay || 0);
+  };
+  NProgress.done = function () {
+    this.clearDelay();
+    this.doDone();
+  };
   // import { loading } from '$lib/others/store';
 
-  NProgress.configure({ showSpinner: false })
+
+  NProgress.configure({ showSpinner: false, minimum: 0.16, easing: 'ease', delay: 100 })
   $: $navigating ? NProgress.start() : NProgress.done()
   // $: $loading ? NProgress.start() : NProgress.done()
 </script>
